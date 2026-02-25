c1136feac7f96c5b0168686c7c11642ba9d7666a
brouqzz <nikita.chekurda2007@gmail.com>
2026-02-25 11:20:20 +0100

    Update route.js

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -52,7 +52,7 @@ export async function POST(request) {
         },
       ],
       mode: "payment",
-      success_url: `${origin}/success`,
+      success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/cancel`,
     });
 
